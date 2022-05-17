6b51bfb8ddab31d1af7cae68284734c48e77919a
gonzacat22 <gonzalodurante22@hotmail.com>
2022-05-17 09:23:50 -0300

    Agragado  Loader

---
+++ js/script.js
@@ -0,0 +1,5 @@
+window.addEventListener('load', () =>{
+    const contenedorLoader = document.querySelector('.contenedorLoader')
+    contenedorLoader.style.opacity = 0
+    contenedorLoader.style.visibility = 'hidden'
+})
